0c85633cbe4eb00d4644bb4cdf79137d774efabd
Vdovicenco Alexandr <vdovicenco.alexandr@gmail.com>
2018-11-10 20:07:12 +0200

    Added basic Imap class

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import 'babel-polyfill'
 import config from './config'
 import express from 'express'
+import Imap from './imap'
 
 let app = express()
 
 app.listen(config.port, () => {
     console.log(`Application is running on port: http://localhost:${config.port}`)
 })
+
+new Imap('grupafaf151@gmail.com', 'GRUPAFAF151ABCD').getInbox(1)
+         .then(messages => {
+           console.log(messages);
+         })
