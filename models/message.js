760e44330c3d7f1f7b6a50293f072f86c7b58a3b
sspatari <spatari.stanislav@gmail.com>
2018-11-11 01:17:19 +0200

    Update Message model

---
+++ models/message.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+let messageSchema = new mongoose.Schema({
+  fromName: { type: String, trim: true },
+  fromMail: { type: String, trim: true },
+  subject: { type: String, trim: true },
+  date: { type: Date, default: Date.now },
+  read: { type: Boolean, default: false },
+  color: { type: String, default: '#ffa500' },
+  content: { type: String, default: 'TestEmptyMessage' }
+},
+{
+  toObject: { getters: true }
+})
+
+let Message = mongoose.model('Message', messageSchema)
+
+export default Message
