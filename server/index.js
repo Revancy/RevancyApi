d9bf8e984d6606c8f2cfbbb6910e3eb68cf79ce1
sspatari <spatari.stanislav@gmail.com>
2018-11-11 02:44:10 +0200

    Comment unnesesary code

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ import 'babel-polyfill'
 import express from 'express'
 import Server from './server'
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 new Server(express)
